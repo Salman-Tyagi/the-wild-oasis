dd4e2bcb2b8ceaa80aa9a9a89eb7b549083b80b1
Salman Tyagi <salmantyagi15@gmail.com>
2023-09-29 14:27:30 +0530

    implemented copy the cabin from copy button

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Set CORS');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
